f7415cc559b648c853a97cd822e5300923dde7d5
gloriiakang <lori.vihchoi@gmail.com>
2021-11-07 22:55:20 -0300

    Ajustando conexão com banco e algumas alterações da tela

---
+++ models.py
@@ -0,0 +1,8 @@
+class Posto:
+    def __init__(self, preco, produto, bairro, nome, bandeira, id=None):
+        self.id = id
+        self.preco = preco
+        self.produto = produto
+        self.bairro = bairro
+        self.nome = nome
+        self.bandeira = bandeira
